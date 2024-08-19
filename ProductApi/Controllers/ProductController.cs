4076cf59242509775094aad051032e23b9eaf29b
tam9999 <tamnguyen20476@gmail.com>
2024-08-19 23:02:52 +0700

    first commit

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(ILogger<ProductController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
